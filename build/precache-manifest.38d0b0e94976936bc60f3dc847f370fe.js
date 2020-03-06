4a25e912c863f742fc6d057786d3d7f7586bc52f
fahmimaulanaay <fahmimaulanaay@gmail.com>
2020-03-06 15:54:28 +0700

    deploy

---
+++ build/precache-manifest.38d0b0e94976936bc60f3dc847f370fe.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa77dcfaad5884c94a74799e5657401c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68eea999e266fddb8885",
+    "url": "/static/css/main.bd4b9eea.chunk.css"
+  },
+  {
+    "revision": "0735e7d8cc1b49d6fb0a",
+    "url": "/static/js/2.83aa09e4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.83aa09e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68eea999e266fddb8885",
+    "url": "/static/js/main.fa2fdfb9.chunk.js"
+  },
+  {
+    "revision": "2fc8e7369f6406e5984d",
+    "url": "/static/js/runtime-main.447fd809.js"
+  },
+  {
+    "revision": "bfbd1fb52d4095f8518fb11a8d5c6f83",
+    "url": "/static/media/Daki.bfbd1fb5.jpg"
+  },
+  {
+    "revision": "e3709d55fe8e2d9159995dccc4008b50",
+    "url": "/static/media/Selam.e3709d55.jpg"
+  },
+  {
+    "revision": "e54cc6b9def391af4d48a42d746b9ffd",
+    "url": "/static/media/Sepeda.e54cc6b9.jpg"
+  },
+  {
+    "revision": "751ba5fe8351cd96b2136a0e677ae656",
+    "url": "/static/media/Sepeda3.751ba5fe.jpg"
+  },
+  {
+    "revision": "97e32961d07c24d899510b8a49887df5",
+    "url": "/static/media/move-logo.97e32961.jpg"
+  },
+  {
+    "revision": "7df9081961b94da6e01d66a96a700f2c",
+    "url": "/static/media/panah.7df90819.jpg"
+  }
+]);
